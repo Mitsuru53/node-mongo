5457985e2e7b5276728c95eee38e3161ca50670c
Mitsuru Hashimoto <exhikkii@gmail.com>
2017-12-29 18:51:56 +0900

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -73,10 +73,6 @@ app.delete('/todos/:id', (req,res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Started up at port ${port}`);
-});
-
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
@@ -102,5 +98,23 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 })
+
+app.post('/users', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+    // res.send(user);
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
 
 module.exports = {app};
